f7efc724bd9e8a633d86625224e02ab25416d777
HasanHuseyin00 <100002059+HasanHuseyin00@users.noreply.github.com>
2022-02-28 18:16:38 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        a:
+            Console.Clear();
+            Console.WriteLine("1-Tas");
+            Console.WriteLine("2-Kagıt");
+            Console.WriteLine("3-Makas");
+            Console.Write("Birini Seciniz:");
+            int x = Convert.ToInt32((Console.ReadLine()));
+            Random rndm = new Random();
+            int y = rndm.Next(1, 4);
+            Console.WriteLine("Bilgisayar Secimi:" + y);
+            if (x == 1 && y == 1)
+                Console.WriteLine("Berabere!");
+            else if (x == 1 && y == 2)
+                Console.WriteLine("Yenildiniz!");
+            else if (x == 1 && y == 3)
+                Console.WriteLine("Kazandınız!");
+            else if (x == 2 && y == 1)
+                Console.WriteLine("Kazandınız!");
+            else if (x == 2 && y == 2)
+                Console.WriteLine("Berabere!");
+            else if (x == 2 && y == 3)
+                Console.WriteLine("Yenildiniz!");
+            else if (x == 3 && y == 1)
+                Console.WriteLine("Yenildiniz!");
+            else if (x == 3 && y == 2)
+                Console.WriteLine("Kazandınız!");
+            else
+                Console.WriteLine("Berabere!");
+            Console.ReadKey();
+            goto a;
+
+
+
+        }
+    }
+}
